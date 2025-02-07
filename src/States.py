cdaee968e3067203dbb2b0d10e74ae8c615d3406
paul-zz <593958169@qq.com>
2025-02-07 20:29:15 +0800

    added switchbutton to the demo

--- src/States.py
+++ src/States.py
@@ -5,6 +5,7 @@ from .AssetsLoader import AssetsLoader
 from .LocaleManager import LocaleManager
 from .OptionBox import OptionBox
 from .PushButton import PushButton
+from .SwitchButton import SwitchButton
 
 class State:
     def handle(self,event):
@@ -46,9 +47,17 @@ class Homepage(State):
         self.list1 = OptionBox(
     0, 0, 300, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), ['One', 'Two', 'Three', '4. Very Long Option'])
         
-        self.button1 = PushButton(
+        self.btn_ok = PushButton(
+    0, 668, 512, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
+    "Confirm")
+        
+        self.btn_cancel = PushButton(
+    512, 668, 512, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
+    "Cancel")
+        
+        self.btn_switch = SwitchButton(
     300, 0, 300, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
-    "BUTTON")
+    "YES", "NO", False)
         
 
     def firstDisplay(self, screen : pygame.Surface):
@@ -68,17 +77,25 @@ class Homepage(State):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit()
         selected_option = self.list1.update(event)
-        button_pressed = self.button1.update(event)
+        btn_ok_pressed = self.btn_ok.update(event)
+        btn_cancel_pressed = self.btn_cancel.update(event)
+        btn_switch_on = self.btn_switch.update(event)
+
         if selected_option >=0:
             # Set locale and refresh the titles
             LocaleManager.setLocale(selected_option)
             self.refreshOnce()
-        if button_pressed:
-            print("PRESSED ON BUTTON 1")
+        if btn_ok_pressed:
+            print("PRESSED ON CONFIRM BUTTON")
+        if btn_cancel_pressed:
+            print("PRESSED ON CANCEL BUTTON")
+
 
     def update(self, game):
         # Clear the background
         self.screen.blit(self.bg, (0, 0))
-        updates = [self.button1.draw(self.screen)]
+        updates = [self.btn_ok.draw(self.screen)]
+        updates.append(self.btn_cancel.draw(self.screen))
+        updates.append(self.btn_switch.draw(self.screen))
         updates.extend(self.list1.draw(self.screen))
         pygame.display.update(updates)

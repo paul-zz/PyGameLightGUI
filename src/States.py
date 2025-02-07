d00287e531e3eb93ba01d28e8121fc401659e4ff
Zijian ZHONG <593958169@qq.com>
2025-02-07 22:40:19 +0800

    updated demo

--- src/States.py
+++ src/States.py
@@ -1,11 +1,14 @@
 import sys
 import pygame
+import random
 
 from .AssetsLoader import AssetsLoader
 from .LocaleManager import LocaleManager
 from .OptionBox import OptionBox
 from .PushButton import PushButton
 from .SwitchButton import SwitchButton
+from .LabelBox import LabelBox
+from .Gauge import Gauge
 
 class State:
     def handle(self,event):
@@ -45,31 +48,28 @@ class Homepage(State):
 
         # Init elements
         self.list1 = OptionBox(
-    0, 0, 300, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), ['One', 'Two', 'Three', '4. Very Long Option'])
+    0, 100, 300, 50, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("smallfont"), ['One', 'Two', 'Three', 'Four. Very Very Long Option Text'])
         
+        self.gauge1 = Gauge(
+    0, 200, 600, 50, (255, 255, 255), (100, 200, 255), 50, 0, 100, True, AssetsLoader.getFont("smallfont"))
+
         self.btn_ok = PushButton(
-    0, 668, 512, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
-    "Confirm")
+    0, 668, 512, 50, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("smallfont"), "Confirm")
         
         self.btn_cancel = PushButton(
-    512, 668, 512, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
-    "Cancel")
+    512, 668, 512, 50, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("smallfont"), "Cancel")
         
         self.btn_switch = SwitchButton(
-    300, 0, 300, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
+    300, 100, 300, 50, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("smallfont"), 
     "YES", "NO", False)
         
+        self.label1 = LabelBox(0, 0, 1024, 80, "LightGUI demo", font=AssetsLoader.getFont("midfont"))
 
     def firstDisplay(self, screen : pygame.Surface):
         # Display only once when the state is created
-        self.refreshOnce()
+        self.screen.blit(self.bg, (0, 0))
+        pygame.display.flip()
     
-    def refreshOnce(self):
-        # Function to refresh the titles once
-        # Trigger only when the titles are changed
-        self.screen.blit(self.bg, (0, 0))       
-        self.list1.draw(self.screen)
-        pygame.display.update()
 
     def handle(self, event):
         if event.type == pygame.QUIT:
@@ -80,11 +80,12 @@ class Homepage(State):
         btn_ok_pressed = self.btn_ok.update(event)
         btn_cancel_pressed = self.btn_cancel.update(event)
         btn_switch_on = self.btn_switch.update(event)
+        gauge_curr_val = self.gauge1.update(event)
 
         if selected_option >=0:
             # Set locale and refresh the titles
             LocaleManager.setLocale(selected_option)
-            self.refreshOnce()
+            self.update(None)
         if btn_ok_pressed:
             print("PRESSED ON CONFIRM BUTTON")
         if btn_cancel_pressed:
@@ -97,5 +98,7 @@ class Homepage(State):
         updates = [self.btn_ok.draw(self.screen)]
         updates.append(self.btn_cancel.draw(self.screen))
         updates.append(self.btn_switch.draw(self.screen))
+        updates.append(self.label1.draw(self.screen))
+        updates.append(self.gauge1.draw(self.screen))
         updates.extend(self.list1.draw(self.screen))
         pygame.display.update(updates)

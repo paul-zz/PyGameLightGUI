0c3442b5c620d1312fee3f9a1324c0ecfcb63424
paul-zz <593958169@qq.com>
2025-02-07 16:12:48 +0800

    added test code for PushButton

--- src/States.py
+++ src/States.py
@@ -4,6 +4,7 @@ import pygame
 from .AssetsLoader import AssetsLoader
 from .LocaleManager import LocaleManager
 from .OptionBox import OptionBox
+from .PushButton import PushButton
 
 class State:
     def handle(self,event):
@@ -35,13 +36,20 @@ class Paused(State):
 class Homepage(State):
     def __init__(self, screen : pygame.Surface):
         self.screen = screen
-        self.finished = False
-        self.list1 = OptionBox(
-    880, 40, 100, 30, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("smallfont"), 
-    LocaleManager.getAllNames())
         self.font_big = AssetsLoader.getFont("bigfont")
         self.font_mid = AssetsLoader.getFont("midfont")
         self.bg = AssetsLoader.getImage("background")
+
+        self.finished = False
+
+        # Init elements
+        self.list1 = OptionBox(
+    0, 0, 300, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), ['One', 'Two', 'Three', '4. Very Long Option'])
+        
+        self.button1 = PushButton(
+    300, 0, 300, 100, (255, 255, 255), (100, 200, 255), AssetsLoader.getFont("midfont"), 
+    "BUTTON")
+        
 
     def firstDisplay(self, screen : pygame.Surface):
         # Display only once when the state is created
@@ -60,13 +68,17 @@ class Homepage(State):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit()
         selected_option = self.list1.update(event)
+        button_pressed = self.button1.update(event)
         if selected_option >=0:
             # Set locale and refresh the titles
             LocaleManager.setLocale(selected_option)
             self.refreshOnce()
+        if button_pressed:
+            print("PRESSED ON BUTTON 1")
 
     def update(self, game):
         # Clear the background
         self.screen.blit(self.bg, (0, 0))
-        updates = self.list1.draw(self.screen)
+        updates = [self.button1.draw(self.screen)]
+        updates.extend(self.list1.draw(self.screen))
         pygame.display.update(updates)
